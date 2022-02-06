8dcd034c718ec465a65278d8779b6cf688021a24
claudiomelendres <claudiomelendres@gmail.com>
2022-02-06 10:12:19 -0400

    debounce

---
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,38 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {debounceTime, Subject} from "rxjs";
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [
+  ]
+})
+export class PaisInputComponent implements OnInit {
+
+  @Output() onEnter: EventEmitter<string> = new EventEmitter<string>();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter<string>();
+
+  debouncer: Subject<string> = new Subject<string>();
+
+  termino: string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+    this.debouncer
+      .pipe(
+        debounceTime(300))
+      .subscribe( valor => {
+        this.onDebounce.emit( valor );
+    })
+  }
+
+  buscar() {
+    this.onEnter.emit( this.termino );
+  }
+
+  teclaPresionada() {
+
+    this.debouncer.next(this.termino);
+  }
+
+}
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -19,8 +19,9 @@ export class PorPaisComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  buscar() {
+  buscar( termino: string) {
     this.hayError = false;
+    this.termino = termino;
     console.log(this.termino)
     this.paisService.buscarPais(this.termino)
       .subscribe( (paises) => {
@@ -32,6 +33,11 @@ export class PorPaisComponent implements OnInit {
         // console.log(err);
         this.hayError = true;
       })
+  }
+
+  sugerencias( termino: string) {
+    this.hayError = false;
+
   }
 
 }
